8c51dc4c594fcb75a918d71aac45a69bfa09c94c
JacekK87 <jacekielar1@wp.pl>
2018-01-31 20:12:33 +0100

    rek

---
+++ cmake-build-debug/CMakeFiles/nwd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nwd.dir/main.c.o"
+  "nwd.pdb"
+  "nwd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nwd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int nwd(int a, int b) {
+    while (b != a)
+        if (b > a)
+            b = b - a;
+        else
+            a = a - b;
+    return a;
+}
+
+int nwd1(int a , int b){
+
+/*	if(b>a)
+		return nwd1 (a, b-a);
+	if (b<a)
+		return (a-b, b);
+	if (a==b)
+		return b;*/
+    if (b==0)
+        return a;
+        else return nwd1(b, a%b);
+}
+		
+int main()
+{
+    int a, b, c ;
+
+    printf("Podaj liczbe a wieksza od 0\n");
+    scanf("%d", &a);
+    printf("Podaj liczbe b wieksza od 0\n");
+    scanf("%d", &b);
+    printf("NWD(%d i %d) = ", a, b);
+
+    c = nwd(a, b);
+    printf("%d\n", c);
+    c = nwd1 (a, b );
+    printf("NWD (%d i %d) = %d\n",a ,b, c);
+    return 0;
+}
